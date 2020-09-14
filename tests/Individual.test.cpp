b1f41ef06941b5068ae2ea41c16561ba97abb9c2
Jerry <Kanegerardo_@hotmail.com>
2020-09-13 22:14:15 -0700

    Adding unit tests for individual

---
+++ tests/Individual.test.cpp
@@ -0,0 +1,29 @@
+#include <catch2.h>
+#include "../src/Individual.cpp"
+
+TEST_CASE("Individual fitness calculation tests")
+{
+
+    SECTION("Individual fitness calculation succeeds")
+    {
+        Individual testIndividual(5, 0, 1);
+        testIndividual.genes = {0, 1, 1, 0, 0};
+        float fitness = testIndividual.calculateFitness([](void *context, const int genes[], const int size) {
+            float sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += genes[i];
+            }
+            return sum;
+        },
+                                                        nullptr);
+
+        REQUIRE(fitness == 2.0f);
+    }
+
+    SECTION("Individual constructor works as expected")
+    {
+        Individual testIndividual(10, 0, 1);
+        REQUIRE(testIndividual.genes.size() == 10);
+    }
+}
--- tests/fakeTest.test.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2.h>
-
-TEST_CASE("testing!!")
-{
-    SECTION("Test pass on purpose")
-    {
-        REQUIRE(1 == 1);
-    }
-}

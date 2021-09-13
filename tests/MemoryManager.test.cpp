ac9d3a89e8b0ec8373dd7dfcf0a44506dbc3eec9
Jerry <Kanegerardo_@hotmail.com>
2021-09-12 22:26:23 -0700

    Adding memory manager tets

--- tests/MemoryManager.test.cpp
+++ tests/MemoryManager.test.cpp
@@ -4,10 +4,11 @@
 TEST_CASE("MemoryManager works as expected")
 {
 
+    int MAX_MEM_SIZE = 500;
+    MemoryManager<int> manager(MAX_MEM_SIZE);
+
     SECTION("MemoryManager creates a maximum of MAX_MEM_SIZE elements")
     {
-        int MAX_MEM_SIZE = 500;
-        MemoryManager<int> manager(MAX_MEM_SIZE);
         int times = MAX_MEM_SIZE;
         while (times--)
         {
@@ -18,22 +19,32 @@ TEST_CASE("MemoryManager works as expected")
         REQUIRE(error == -1);
     }
 
-    // SECTION("MemoryManager destroys memory and allows new creations")
-    // {
-    //     int times = MAX_MEM_SIZE;
-    //     while (times--)
-    //     {
-    //         manager.destroy(times);
-    //     }
+    SECTION("MemoryManager destroys memory and allows new creations")
+    {
+        int times = MAX_MEM_SIZE;
+        while (times--)
+        {
+            manager.destroy(times);
+        }
 
-    //     times = MAX_MEM_SIZE;
-    //     while (times--)
-    //     {
-    //         int handle = manager.create(times);
-    //         REQUIRE(handle != -1);
-    //     }
+        times = MAX_MEM_SIZE;
+        while (times--)
+        {
+            int handle = manager.create(times);
+            REQUIRE(handle != -1);
+        }
+    }
 
-    //     int handle = manager.create(999);
-    //     REQUIRE(handle == -1);
-    // }
+    SECTION("MemoryManager returns -1 when there is no memory left")
+    {
+        int times = MAX_MEM_SIZE;
+        while (times--)
+        {
+            int handle = manager.create(times);
+            REQUIRE(handle != -1);
+        }
+
+        int handle = manager.create(999);
+        REQUIRE(handle == -1);
+    }
 }
